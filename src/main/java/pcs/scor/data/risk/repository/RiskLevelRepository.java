b75dabe422b9c431047c7dcae516e684dc486353
Art Gonzalez <arturo.gonzalezjr@hhs.texas.gov>
2023-04-28 00:29:38 -0500

    not needed

--- src/main/java/pcs/scor/data/risk/repository/RiskLevelRepository.java
+++
@@ -1,9 +0,0 @@
-package pcs.scor.data.risk.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pcs.scor.domain.risk.tmpl.RiskLevelEntity;
-
- public interface RiskLevelRepository extends JpaRepository<RiskLevelEntity, Integer> {
-	
-}
