a85b37f8b3d544740b12a0fafb2a62922c84efa9
Art Gonzalez <arturo.gonzalezjr@hhs.texas.gov>
2023-05-03 21:14:05 -0500

    clean up imports

--- src/main/java/pcs/scor/web/RiskAssessmentController.java
+++ src/main/java/pcs/scor/web/RiskAssessmentController.java
@@ -1,7 +1,5 @@
 package pcs.scor.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.PagedResources;
